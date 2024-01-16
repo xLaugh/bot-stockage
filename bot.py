73320504eafeb5092e0117781889f3284db44300
Laugh <70723697+xLaugh@users.noreply.github.com>
2024-01-16 11:51:15 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,10 +3,10 @@ from discord.ext import commands
 import mysql.connector
 
 TOKEN = 'TOKEN'
-DB_HOST = 'localhost'
-DB_USER = 'root'
-DB_PASSWORD = 'root'
-DB_NAME = 'bdd'
+DB_HOST = 'host'
+DB_USER = 'user'
+DB_PASSWORD = 'password'
+DB_NAME = 'vagos'
 
 intents = discord.Intents.all()
 
@@ -35,12 +35,12 @@ cursor.execute('''
 ''')
 conn.commit()
 
-coffre_channel_id = ID_CHANNEL
+coffre_channel_id = channel_id
 
 async def update_coffre_message():
     channel = bot.get_channel(coffre_channel_id)
     if channel:
-        cursor.execute('SELECT * FROM coffre')
+        cursor.execute('SELECT * FROM coffre WHERE quantity > 0')
         items = cursor.fetchall()
 
         if not items:
@@ -50,7 +50,7 @@ async def update_coffre_message():
         embed = discord.Embed(title='Coffre', color=0xFFA500)
 
         for item in items:
-            embed.add_field(name=item[0], value=f"Quantité: {item[1]}", inline=False)
+            embed.add_field(name=item[0], value=f"QuantitÃ©: {item[1]}", inline=False)
 
         async for message in channel.history(limit=100):
             if message.author == bot.user:
@@ -62,10 +62,18 @@ async def update_coffre_message():
 @bot.command(name='add')
 @commands.has_permissions(administrator=True)
 async def add_item(ctx, *, item_name):
-    cursor.execute('INSERT INTO items (item_name) VALUES (%s)', (item_name,))
-    conn.commit()
-    await ctx.send(f'Item ajouté à la base de données : {item_name}')
-    await update_coffre_message()
+    # VÃ©rifier si l'item existe dÃ©jÃ  dans la base de donnÃ©es
+    cursor.execute('SELECT item_name FROM items WHERE item_name = %s', (item_name,))
+    item_exists = cursor.fetchone()
+
+    if item_exists:
+        await ctx.send(f"L'item {item_name} existe dÃ©jÃ  dans la base de donnÃ©es.")
+    else:
+        # Ajouter l'item Ã  la base de donnÃ©es
+        cursor.execute('INSERT INTO items (item_name) VALUES (%s)', (item_name,))
+        conn.commit()
+        await ctx.send(f'Item ajoutÃ© Ã  la base de donnÃ©es : {item_name}')
+        await update_coffre_message()
 
 @bot.command(name='ajouter')
 async def ajouter_item(ctx, item_name, quantity: int):
@@ -75,16 +83,16 @@ async def ajouter_item(ctx, item_name, quantity: int):
     if item_exists:
         cursor.execute('INSERT INTO coffre (item_name, quantity) VALUES (%s, %s) ON DUPLICATE KEY UPDATE quantity = quantity + %s', (item_name, quantity, quantity))
         conn.commit()
-        await ctx.send(f'Item ajouté au coffre : {item_name} (Quantité: {quantity})')
+        await ctx.send(f'Item ajoutÃ© au coffre : {item_name} (QuantitÃ©: {quantity})')
         await update_coffre_message()
     else:
-        await ctx.send(f"L'item {item_name} n'existe pas dans la base de données.")
+        await ctx.send(f"L'item {item_name} n'existe pas dans la base de donnÃ©es.")
 
 @bot.command(name='supprimer')
 async def supprimer_item(ctx, item_name, quantity: int):
     cursor.execute('UPDATE coffre SET quantity = GREATEST(quantity - %s, 0) WHERE item_name = %s', (quantity, item_name))
     conn.commit()
-    await ctx.send(f'Item supprimé du coffre : {item_name} (Quantité: {quantity})')
+    await ctx.send(f'Item supprimÃ© du coffre : {item_name} (QuantitÃ©: {quantity})')
     await update_coffre_message()
 
 @bot.command(name='liste')
@@ -93,7 +101,7 @@ async def stockage(ctx):
     items = cursor.fetchall()
 
     if not items:
-        await ctx.send('Aucun item trouvé dans la base de données.')
+        await ctx.send('Aucun item trouvÃ© dans la base de donnÃ©es.')
         return
 
     embed = discord.Embed(title='Liste des items', color=0xFFA500)
